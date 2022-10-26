c5cd2875096ff0b86785889be87506fd9cf98ffd
Isabela Franca Novais <isabela_0405@hotmail.com>
2022-10-26 14:51:15 -0300

    novos codigos

--- tensao_min/teste.py
+++ tensao_min/teste.py
@@ -15,16 +15,27 @@ caminho = filedialog.askdirectory()
 
 arquivos_pasta = os.listdir(fr"{caminho}")
 alimentadores = list()
+titulo = list()
 
-# reading CSV file
+for i in range(len(arquivos_pasta)):
+    alimentadores.append(arquivos_pasta[i])
+    # reading CSV file
+    perdas_alimentadores = pd.read_csv(fr"{caminho}/{alimentadores[i]}", delimiter=',')
+    pd.concat(perdas_alimentadores[i])
+
+    # converting column data to list
+    titulo = perdas_alimentadores['Arquivo'].tolist()
+    # T = titulo.split("_")
+    energia_ativa = perdas_alimentadores['Energia ativa (MWh)'].tolist()
+    perdas = perdas_alimentadores['Perdas (MWh)'].tolist()
+
+    i+=1
+
+
 #data = pd.read_csv(r"ACL01C1_5678_OpenDSS.csv")
-perdas_alimentadores = pd.read_csv(r'C:\Users\Isabela\Documents\P&D EQUATORIAL\FINAL\Caso_0\Caso_0\ACL01C1_5678_OpenDSS.csv', delimiter=',')
 
-# # converting column data to list
-titulo = perdas_alimentadores['Arquivo'].tolist()
-T = titulo.split("_")
-energia_ativa = perdas_alimentadores['Energia ativa (MWh)'].tolist()
-perdas = perdas_alimentadores['Perdas (MWh)'].tolist()
+
+
 
 # for a in range (12):
 
